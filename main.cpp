644502a60ee66d27297fd19947b60cc6487bbf18
goderyu <goderyu@openzi.com>
2023-08-23 00:40:28 +0800

    新增：- Windows平台下集成Crashpad库

--- main.cpp
+++ main.cpp
@@ -27,26 +27,40 @@ void thread_write() {
     }
 }
 
+
 int main(int argc, char** argv) {
+#if OS_LINUX
     string exeDir = std::filesystem::current_path().u8string();
     base::FilePath handler(exeDir + "/crashpad_handler");
     base::FilePath reportsDir(exeDir + "/Crashes");
     base::FilePath metricsDir(exeDir + "/Crashes");
+    string url = "";
     map<string, string> annotations;
-    string url = "https://133077719_qq_com.bugsplat.com/post/bp/crash/crashpad.php";
     annotations["format"] = "minidump";
-    annotations["database"] = "133077719_qq_com";
-    annotations["product"] = "OpenZIEditorLauncher";
+    annotations["product"] = "CrashDemo";
     annotations["version"] = "1.0.0";
-    annotations["key"] = "Sample key";
-    annotations["user"] = "goderyu@cengzi.com";
-    annotations["list_annotations"] = "Sample comment";
-
     vector<string> arguments;
     arguments.emplace_back("--no-rate-limit");
     vector<base::FilePath> attachments;
     base::FilePath attachment(exeDir + "/Crashes/attachment.txt");
     attachments.emplace_back(attachment);
+#elif OS_WIN
+    wstring exeDir = std::filesystem::current_path().wstring();
+    base::FilePath handler(exeDir + L"/crashpad_handler.exe");
+    base::FilePath reportsDir(exeDir + L"/Crashes");
+    base::FilePath metricsDir(exeDir + L"/Crashes");
+    string url = "";
+    map<string, string> annotations;
+    annotations["format"] = "minidump";
+    annotations["product"] = "CrashDemo";
+    annotations["version"] = "1.0.0";
+    vector<string> arguments;
+    arguments.emplace_back("--no-rate-limit");
+    vector<base::FilePath> attachments;
+    base::FilePath attachment(exeDir + L"/Crashes/attachment.txt");
+    attachments.emplace_back(attachment);
+#endif
+
     unique_ptr<crashpad::CrashReportDatabase> database =
         crashpad::CrashReportDatabase::Initialize(reportsDir);
     if (database == nullptr) {
@@ -56,16 +70,22 @@ int main(int argc, char** argv) {
     crashpad::Settings* settings = database->GetSettings();
     settings->SetUploadsEnabled(false);
     crashpad::CrashpadClient* client = new crashpad::CrashpadClient();
+#if OS_LINUX
     client->StartHandlerAtCrash(handler, reportsDir, metricsDir, url, annotations, arguments);
+#elif OS_WIN
+    client->StartHandler(handler, reportsDir, metricsDir, url, annotations, arguments, false, false, attachments);
+#endif
     // // test nullptr assign
-    // int* a = (int*)nullptr;
-    // *a = 1;
+    cout << "Wait for crash..." << endl;
+    this_thread::sleep_for(chrono::seconds(3));
+    int* a = (int*)nullptr;
+    *a = 1;
 
     // test multi-thread read and write without lock
-    thread write_thread(thread_write);
-    thread read_thread(thread_read);
-    write_thread.join();
-    read_thread.join();
+    // thread write_thread(thread_write);
+    // thread read_thread(thread_read);
+    // write_thread.join();
+    // read_thread.join();
 
     this_thread::sleep_for(chrono::seconds(10));
 }
